866fa4f37679c5f44c92f15ef14440f277ae78a3
VitorAvelar1 <vitorhugo.diniz@hotmail.com>
2025-04-09 21:20:43 -0300

    Coloquei a conversão do Euro e Dólar, usando o if.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-dolar-euro")
 
 function convertValues() {
     const inputValue = document.querySelector(".input-value").value
     const valueConvert = document.querySelector(".p-Valor-convert")
     const valueConverted = document.querySelector(".p-Valor")
 
+    console.log(currencySelect.value)
     const dolarToday = 5.2
+    const euroToday = 6.2
 
-    const convertedValue = inputValue / dolarToday
+    if (currencySelect.value == "dolar") {
+        valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+        valueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValue / euroToday)
+    }
 
     valueConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style:"currency",
-        currency:"BRL"
+        style: "currency",
+        currency: "BRL"
     }).format(inputValue)
 
-    valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style:"currency",
-        currency:"USD"
-    }).format(convertedValue)
-
-    console.log(convertedValue)
 }
 
- convertButton.addEventListener("click", convertValues)
+convertButton.addEventListener("click", convertValues)
