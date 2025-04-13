7fc38a1de3ba76dd8cf8acbe0f986ccade19daf0
VitorAvelar1 <vitorhugo.diniz@hotmail.com>
2025-04-13 12:28:59 -0300

    Refiz toda a operação no JS para rever e assimilar mais os códigos desse projeto.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,23 @@
 const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-dolar-euro")
+const selectCurrency = document.querySelector(".currency-dolar-euro")
 
 function convertValues() {
-    const inputValue = document.querySelector(".input-value").value
-    const valueConvert = document.querySelector(".p-Valor-convert")
-    const valueConverted = document.querySelector(".p-Valor")
+    const inputCurrency = document.querySelector(".input-value").value
+    const currencyConvert = document.querySelector(".p-Valor-convert")
+    const curencyConverted = document.querySelector(".p-Valor")
 
-    console.log(currencySelect.value)
     const dolarToday = 5.2
     const euroToday = 6.2
 
-    if (currencySelect.value == "dolar") {
-        valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-        }).format(inputValue / dolarToday)
+    if (selectCurrency.value == "dolar") {
+        curencyConverted.innerHTML = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(inputCurrency / dolarToday)
     }
 
-    if (currencySelect.value == "euro") {
-        valueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR"
-        }).format(inputValue / euroToday)
+    if (selectCurrency.value == "euro") {
+        curencyConverted.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(inputCurrency / euroToday)
     }
 
-    valueConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(inputValue)
+    currencyConvert.innerHTML = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(inputCurrency)
 
 }
 
@@ -35,12 +25,12 @@ function changeCurrency() {
     const currencyName = document.querySelector(".p-moeda-dolar")
     const currencyImage = document.querySelector(".img-Moeda-dolar")
 
-    if (currencySelect.value == "dolar"){
+    if (selectCurrency.value == "dolar") {
         currencyName.innerHTML = "Dólar americano"
         currencyImage.src = "./assets/estados-unidos (1) 1.png"
     }
 
-    if (currencySelect.value == "euro"){
+    if (selectCurrency.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImage.src = "./assets/Euro.png"
     }
@@ -48,5 +38,5 @@ function changeCurrency() {
     convertValues()
 }
 
-currencySelect.addEventListener("change", changeCurrency)
+selectCurrency.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
