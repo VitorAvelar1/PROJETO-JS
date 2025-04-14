9b34dc67aa5d7de3b1c4294566cce04afd3d759d
VitorAvelar1 <vitorhugo.diniz@hotmail.com>
2025-04-14 10:11:52 -0300

    Acrescentei mais duas moedas, Libra e Lira Turca.

--- scripts.js
+++ scripts.js
@@ -6,8 +6,10 @@ function convertValues() {
     const currencyConvert = document.querySelector(".p-Valor-convert")
     const curencyConverted = document.querySelector(".p-Valor")
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    const dolarToday = 5.87
+    const euroToday = 6.68
+    const libraToday = 7.69
+    const liraTurcaToday = 6.75
 
     if (selectCurrency.value == "dolar") {
         curencyConverted.innerHTML = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(inputCurrency / dolarToday)
@@ -15,6 +17,14 @@ function convertValues() {
 
     if (selectCurrency.value == "euro") {
         curencyConverted.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(inputCurrency / euroToday)
+    }
+
+    if (selectCurrency.value == "libra") {
+        curencyConverted.innerHTML = new Intl.NumberFormat("lb-LB", { style: "currency", currency: "GBP" }).format(inputCurrency / libraToday)
+    }
+
+    if (selectCurrency.value == "lira") {
+        curencyConverted.innerHTML = new Intl.NumberFormat("tl-TL", { style: "currency", currency: "TRY" }).format(inputCurrency / liraTurcaToday)
     }
 
     currencyConvert.innerHTML = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(inputCurrency)
@@ -33,6 +43,16 @@ function changeCurrency() {
     if (selectCurrency.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImage.src = "./assets/Euro.png"
+    }
+
+    if (selectCurrency.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra 1.png"
+    }
+
+    if (selectCurrency.value == "lira") {
+        currencyName.innerHTML = "Lira"
+        currencyImage.src = "./assets/png-liraturca.png"
     }
 
     convertValues()
